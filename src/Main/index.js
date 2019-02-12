80eaedfc5b62fc7a935a8bae1483c8a2e5f765a3
Ak1la <acila1996@gmail.com>
2019-02-12 18:54:07 +0200

    Start with form

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Users from '../Users';
 
-export default function Main (){
-    return (
-        <div className="container">
-             <div className="row">
-                <div className="col"><Users/></div>
-                 <div className="col"></div>
-             </div>
-         </div>
-    )
+export default class Main extends Component {
+   
+    state = {
+        choosUserId: null
+    }
+
+     render(){ 
+        return (
+            <div className="container">
+                  <div className="row">
+                     <div className="col"><Users chooseUserID={this.chooseUserID.bind(this)}/></div>
+                     <div className="col"></div>
+                 </div>
+              </div>
+        )
+    }
+
+    chooseUserID = (chooseUserID) => this.setState({
+        chooseUserID: this.state.chooseUserID === chooseUserID ? null : chooseUserID
+    })
+
 }
